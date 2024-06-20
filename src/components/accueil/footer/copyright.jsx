bce57010c3a60a316e19d719e140e61d0d95725d
Mbarga <emmanuelmbarg@gmail.com>
2024-06-20 04:21:55 +0100

    creation de ficho=ier copyRight et modification du footer

---
+++ src/components/accueil/footer/copyright.jsx
@@ -0,0 +1,9 @@
+export const Copyright = () => {
+  return (
+    <>
+      <p className="text-white text-center font-inter mt-4 text-base MiniPortable:text-xs mobil:text-xs Tablette:text-sm Laptop:text-sm">
+        © 2024 | Copyright | Emmanuel Mbarga , Tous droits reservé
+      </p>
+    </>
+  );
+};
--- src/components/accueil/footer/footer.jsx
+++ src/components/accueil/footer/footer.jsx
@@ -1,13 +1,13 @@
 import FooterNavigation from "./footerNavigation";
 import Footertitle from "./footertitle";
-
+import Copyright from "./copyright.jsx"
 export default function FooterApp() {
   return (
     <>
       <footer className="max-w-[1700px] m-auto bg-black py-10 Tablette:max-w-[1000px]">
         <Footertitle/>
         <FooterNavigation />
-        <p className="text-white text-center font-inter mt-4 text-base MiniPortable:text-xs mobil:text-xs Tablette:text-sm Laptop:text-sm">© 2024 | Copyright | Emmanuel Mbarga , Tous droits reservé</p>
+        <Copyright/>
       </footer>
     </>
   );
