75bad01a2e3e74b5dc2a4fd4d96147a7f2e65e05
Mbarga <emmanuelmbarg@gmail.com>
2024-06-20 06:32:48 +0100

    modification du style et ajout du composant card pour les tablettes

--- src/components/Artciles/sectionoption/btnCategory.jsx
+++ src/components/Artciles/sectionoption/btnCategory.jsx
@@ -9,10 +9,10 @@ export const BtnCategory = (prop) => {
   return (
     <>
       <div
-        className="flex gap-2 items-center p-2 transition hover:border-b  hover:bg-yellow-500 mobil:hover:border-none mobil:hover:bg-white mobil:w-[220px] mobil:border-r mobil:px-4 "
+        className="flex gap-2 items-center p-2 transition hover:border-b  hover:bg-yellow-500 screenLarge:hover:text-white Laptop:hover:text-white mobil:hover:text-black mobil:hover:border-none mobil:hover:bg-white mobil:w-[220px] mobil:border-r mobil:px-4 "
         onClick={handlerClicked}
       >
-        <button className="border border-blue-500 w-4 h-4 rounded-full Tablette:w-2 Tablette:h-2 mobil:w-3 mobil:h-3 MiniPortable:w-1 MiniPortable:h-1"></button>
+        <button className=" bg-blue-500 shadow-lg w-4 h-4 rounded-full Tablette:w-2 Tablette:h-2 mobil:w-3 mobil:h-3 MiniPortable:w-1 MiniPortable:h-1"></button>
         <p className="text-sm uppercase font-inter Tablette:text-xs mobil:text-xs MiniPortable:text-xs">{prop.AllCategori}</p>
       </div>
     </>
--- src/components/accueil/Header/headerComponent1.jsx
+++ src/components/accueil/Header/headerComponent1.jsx
@@ -1,3 +1,4 @@
+import PannierElm from "../../../containers/pannier/pannier";
 import ComponentLogo from "./componentLogo";
 import ComponentSearch from "./componentSearch";
 
@@ -7,6 +8,9 @@ export default function HeaderComponent1() {
       <div className="flex justify-start py-8 items-center px-8 Tablette:py-2 Tablette:px-2 mobil:px-1 mobil:py-2 mobil:justify-around MiniPortable:flex-col MiniPortable:py-2 MiniPortable:px-1">
         <ComponentLogo />
         <ComponentSearch />
+        <div className="mobil:hidden MiniPortable:hidden Laptop:hidden screenLarge:hidden">
+        <PannierElm/>
+        </div>
       </div>
     </>
   );
